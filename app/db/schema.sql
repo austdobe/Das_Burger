bd89676a0327da82bdea88d41145f1d87aca535d
Austin <austin.dober@gmail.com>
2019-10-05 11:53:51 -0400

    adding basic files

---
+++ app/db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS Burger_db;
+
+CREATE DATABASE Burger_db;
+
+use Burger_db;
+
+create table burgers{
+    id int auto_increment,
+    burger_name varchar(100) not null,
+    devoured boolean
+    primary key (id)
+}
